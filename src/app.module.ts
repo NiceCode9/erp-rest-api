46824314f6c4e9cd2522618ff155cbe84a667deb
NiceCode9 <nicecode9@gmail.com>
2026-03-20 22:38:33 +0700

    konfigurasi awal project nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE', 'sqlite') as any,
+        database: configService.get<string>('DB_FILE', 'database.sqlite'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Berbahaya untuk production, tapi oke untuk setup awal/dev
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
